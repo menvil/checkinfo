4ffafc58e9b88c3d358dea8e8392d9e70762379c
Ivan Moroz <menvil.menvil@gmail.com>
2020-05-05 23:21:33 +0300

    change sitemap amount

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,8 +1,6 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @for ($i = 1 ; $i <= $pages; $i++)
-        <sitemap>
-            <loc>{{action('SitemapController@xml', $i)}}</loc>
-        </sitemap>
-    @endfor
+@for ($i = 1 ; $i <= $pages; $i++)
+<sitemap><loc>{{action('SitemapController@xml', $i)}}</loc></sitemap>
+@endfor
 </sitemapindex>
